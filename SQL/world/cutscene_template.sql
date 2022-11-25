1c677d2f3315587364b2fabe9ac42629532dfd72
trickerer <onlysuffering@gmail.com>
2022-11-25 15:06:11 +0700

    Remove unneded sqls, fix conditions crash

--- SQL/world/cutscene_template.sql
+++
@@ -1,4 +0,0 @@
---
-INSERT INTO `cutscene_template` VALUES ('6', '1', '5000', '0', 'test6');
-INSERT INTO `cutscene_template` VALUES ('5', '1', '16000', '0', 'test5');
-INSERT INTO `cutscene_template` VALUES ('1', '571', '21000', '0', 'quest213000');
